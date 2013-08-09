e52542ec3d8cb7da46a224a8e193e1fac982ba61
tdcwilliams <tdcwilliams@gmail.com>
2013-08-09 10:58:59 +0200

    1st commit

---
+++ OcMod2013_paper_code/FB_wave_height_Raleigh_truncdist.m
@@ -0,0 +1,12 @@
+function [P,jbad]=FB_wave_height_Raleigh_truncdist(X,H_s)
+%% CALL: P=FB_wave_height_Raleigh_truncdist(X,H_s),
+%% where H_s is the significant wave height,
+%% and P=probability of getting a wave AMPLITUDE bigger than X;
+%% truncated Rayleigh dist, so no rogue waves (P(2*X>2*Hs)=0); 
+
+P  = 0*X;
+jj = find(X<=H_s);
+%%
+sig_sq   = (H_s/4)^2;
+A        = 1 - exp(-H_s.^2/2/sig_sq);
+P(jj)    = ( exp(-X(jj).^2/2/sig_sq) - exp(-H_s.^2/2/sig_sq) )/A;
